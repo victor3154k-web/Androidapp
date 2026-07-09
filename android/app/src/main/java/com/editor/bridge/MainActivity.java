4571a69eaa08791a6339ce56a450f82b70f6a34d
Victor <victor3154k@gmail.com>
2026-07-09 15:22:45 -0300

    Compilar Bridge Nativo

--- android/app/src/main/java/com/editor/bridge/MainActivity.java
+++ android/app/src/main/java/com/editor/bridge/MainActivity.java
@@ -1,5 +1,25 @@
 package com.editor.bridge;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            // Diminui o zoom da interface para 85%
+            webView.getSettings().setTextZoom(85);
+            // Força o comportamento de tela ampla (estilo desktop)
+            webView.getSettings().setUseWideViewPort(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+        }
+    }
+}
